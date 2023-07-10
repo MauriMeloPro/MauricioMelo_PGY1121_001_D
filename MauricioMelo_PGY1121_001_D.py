d0d1d4be8d0677f043fa08aa8dd07c7e80b867ca
MauriMeloPro <135031243+MauriMeloPro@users.noreply.github.com>
2023-07-10 10:01:32 -0400

    Add files via upload

---
+++ MauricioMelo_PGY1121_001_D.py
@@ -0,0 +1,73 @@
+import os
+os.system ("cls")
+
+menu = ...
+
+ComprarEntradas = 0
+UbicacionesDisponibles = 0
+ListadoAsistentes = 0
+TipoEntradas = 0
+CantidadEntradas = 0
+GananciasTotales = 0
+Run = 0
+Salir = 0
+
+
+Ubicaciones_D = ...
+
+"1,2,3,4,5,6,7,8,9,10,11,12,13"
+"14,15,16,17,18,19,20,21,22,21"
+"22,23,24,25,26,27,28,29,30,31"
+"32,33,34,35,36,37,38,39,40,41"
+"42,43,44,45,46,47,48,49,50,51"
+"52,53,54,55,56,57,58,59,60,61"
+"62,63,64,65,66,67,68,69,70,71"
+"72,73,74,75,76,77,78,79,80,81"
+"82,83,84,85,86,87,88,89,90,91"
+"92,93,94,95,96,97,98,99,100,SALIDA"
+
+print("13,64,19,44,77,22,72,1 VENDIDAS")
+
+print("Porfavor,Ingrese su Run")
+
+while True(input("fPorfavor,Ingrese su Run")):
+
+def ComprarEntradas()
+
+
+ListaPrecios = 0
+
+Platinum = 120.000 
+Gold = 80.000 
+Silver = 50.000 
+
+print ("ListadoAsistentes")
+
+print("Marcos Silva RUN = 134431201")
+print("Cecilia Monsalve RUN = 129925506")
+print("Marcelo Silva  RUN = 191029034")
+print("Rodrigo Veloso RUN = 203235404")
+print("Sergio Salinas RUN = 215301059")
+
+
+def TipoEntradas()
+
+Platinum = 120000
+Gold = 80000
+Silver = 50000
+
+def CantidadEntradas()
+
+Platinum = 2
+Gold = 4
+Silver = 10
+Total = 16
+
+
+def GananciasTotales()
+
+Platinum = 240000
+Gold = 320000
+Silver = 500000
+Total = 1060000
+
